9d7ff1137f9836487a2747573744a920104cc14b
Marco Espinoza <marco.mj.espinoza@gmail.com>
2025-01-18 03:40:34 -0800

    Refactor mock network manager to its own folder

--- ios-assignment/ios-assignmentTests/RecipeListViewModelTests/RecipeListViewModelTests.swift
+++ ios-assignment/ios-assignmentTests/RecipeListViewModelTests/RecipeListViewModelTests.swift
@@ -9,35 +9,6 @@ import XCTest
 @testable import ios_assignment
 
 
-enum NetworkErrorType: CaseIterable {
-    case invalidURL
-    case invalidResponse
-    case decodingError
-}
-
-
-class MockNetworkManager: NetworkManagerProtocol {
-    var errorType: NetworkErrorType?
-    var recipesResponse: [Recipe] = []
-    
-    func getRecipes() async throws -> [Recipe] {
-        if let errorType = self.errorType {
-            switch errorType {
-            case .invalidURL:
-                throw AppError.invalidURL
-            case .invalidResponse:
-                throw AppError.invalidResponse
-            case .decodingError:
-                throw AppError.decodingError
-            }
-        }
-        
-        return self.recipesResponse
-    }
-}
-
-
-
 final class RecipeListViewModelTests: XCTestCase {
     
     var viewModel: RecipeListViewModel!
