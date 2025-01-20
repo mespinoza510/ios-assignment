87a87bca363c69ecc351e4c0e8c8dc93f0426ba0
Marco Espinoza <marco.mj.espinoza@gmail.com>
2025-01-20 03:13:08 -0800

    Add refresh feature to allow pagination and refreshing list

--- ios-assignment/ios-assignment/Screens/RecipeListScreen/RecipeListView.swift
+++ ios-assignment/ios-assignment/Screens/RecipeListScreen/RecipeListView.swift
@@ -7,36 +7,41 @@
 
 import SwiftUI
 
+
 struct RecipeListView: View {
-    
     @StateObject var viewModel = RecipeListViewModel()
     
     var body: some View {
-         NavigationView {
+        NavigationView {
             VStack {
-                if self.viewModel.isLoading {
+                if viewModel.isLoading {
                     ProgressView("Loading...")
-                } else if self.viewModel.recipes.isEmpty {
+                } else if viewModel.visibleRecipes.isEmpty {
                     RecipesUnavailableView()
                 } else {
                     List {
-                        ForEach(self.viewModel.recipes, id: \.uuid) { recipe in
+                        ForEach(viewModel.visibleRecipes, id: \.uuid) { recipe in
                             NavigationLink {
                                 RecipeDetailView(viewModel: RecipeDetailViewModel(recipe: recipe))
                             } label: {
                                 RecipeCell(recipe: recipe)
                             }
                         }
+                        RefreshButtonView(viewModel: self.viewModel)
                     }
                     .listStyle(.plain)
+                    .refreshable {
+                        await self.viewModel.fetchAllRecipes(isRefresh: true)
+                    }
                 }
             }
             .navigationTitle(Text("Recipes"))
         }
-         .alert(item: self.$viewModel.alertContext.alertType) { $0.alertItem.alert }
-        .task { await self.viewModel.fetchRecipes() }
+        .alert(item: $viewModel.alertContext.alertType) { $0.alertItem.alert }
+        .task { await viewModel.fetchAllRecipes() }
     }
 }
+
 
 #Preview {
     RecipeListView()
@@ -49,6 +54,25 @@ struct RecipesUnavailableView: View {
     var body: some View {
         VStack {
             ContentUnavailableView("No recipes found", systemImage: "text.page.badge.magnifyingglass")
+        }
+    }
+}
+
+struct RefreshButtonView: View {
+    @ObservedObject var viewModel: RecipeListViewModel
+    
+    var body: some View {
+        if !self.viewModel.allRecipes.isEmpty && !self.viewModel.isLoading && !self.viewModel.isRefreshing {
+            HStack  {
+                Spacer()
+                Button {
+                    self.viewModel.loadNextPage()
+                } label: {
+                    Text("Load More")
+                        .foregroundStyle(.blue)
+                }
+                Spacer()
+            }
         }
     }
 }
--- ios-assignment/ios-assignment/Screens/RecipeListScreen/RecipeListViewModel.swift
+++ ios-assignment/ios-assignment/Screens/RecipeListScreen/RecipeListViewModel.swift
@@ -13,7 +13,11 @@ final class RecipeListViewModel: ObservableObject {
     
     @Published var alertContext = AlertContext()
     @Published var isLoading: Bool = false
-    @Published var recipes: [Recipe] = []
+    @Published var isRefreshing: Bool = false
+    @Published var allRecipes: [Recipe] = []
+    @Published var visibleRecipes: [Recipe] = []
+    
+    private let pageSize: Int = 25
     
     private let networkManager: NetworkManagerProtocol
     
@@ -21,12 +25,20 @@ final class RecipeListViewModel: ObservableObject {
         self.networkManager = networkManager
     }
     
-    func fetchRecipes() async {
-        self.showLoadingView()
+    /// fetch all recipes with option to refresh
+    func fetchAllRecipes(isRefresh: Bool = false) async {
+        if isRefresh {
+            self.visibleRecipes.removeAll()
+            self.isRefreshing = true
+        } else {
+            self.showLoadingView()
+        }
         
         do {
-            self.recipes = try await self.networkManager.getRecipes()
-            self.hideLoadingView()
+            let fetchedRecipes = try await networkManager.getRecipes()
+            self.allRecipes = fetchedRecipes
+            // Add the first batch of recipes to visibleRecipes
+            self.appendVisibleRecipes(from: self.allRecipes)
         } catch let error as AppError {
             self.hideLoadingView()
             self.alertContext.showAlert(error)
@@ -34,6 +46,34 @@ final class RecipeListViewModel: ObservableObject {
             self.hideLoadingView()
             self.alertContext.showAlert(.decodingError)
         }
+        self.hideLoadingView()
+        self.isRefreshing = false
+    }
+    
+    func loadNextPage() {
+        guard !self.isLoading, !self.allRecipes.isEmpty else { return } // Prevent loading during an active load
+
+        // Determine the number of items to load
+        let recipesToLoad = min(pageSize, allRecipes.count)
+
+        // Extract the next set of recipes and ensure uniqueness
+        let nextBatch = self.allRecipes.prefix(recipesToLoad)
+        let uniqueBatch = nextBatch.filter { !self.visibleRecipes.contains($0) }
+        
+        // update visible and remaining recipes
+        self.visibleRecipes.append(contentsOf: uniqueBatch)
+        self.allRecipes.removeFirst(recipesToLoad)
+    }
+    
+    private func resetData() {
+        self.allRecipes.removeAll()
+        self.visibleRecipes.removeAll()
+    }
+    
+    private func appendVisibleRecipes(from recipes: [Recipe]) {
+        let initialBatch = recipes.prefix(self.pageSize)
+        self.visibleRecipes.append(contentsOf: initialBatch)
+        self.allRecipes = Array(recipes.dropFirst(self.pageSize))
     }
     
     // MARK: - Loading state handlers
